81752692937cafcde23f3c9ec993a3293c1e55e2
Biccira <makstur05@bk.ru>
2024-08-05 20:35:53 +0700

    Add files via upload

---
+++ Basic data structures.py
@@ -0,0 +1,7 @@
+print(9**0.5*5) # 1st program
+print(9.99>9.98 and 1000!=1000.1) # 2st program
+print(2*2+2==(2+2)*2) # 3st program
+one='123.456' # 4st program
+a=float(one)*10 # 4st program
+b=int(a)-1230 # 4st program
+print(b) # 4st program
